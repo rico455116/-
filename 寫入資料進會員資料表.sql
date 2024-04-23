980aa94d3285d0fc549478911ade8d13506051d1
rico455116 <155893119+rico455116@users.noreply.github.com>
2024-04-23 10:27:20 +0800

    Add files via upload

---
+++ 寫入資料進會員資料表.sql
@@ -0,0 +1,5 @@
+INSERT INTO member_info VALUES('110B13098', 'Female', '165', '50');
+INSERT INTO member_info VALUES('110B07167', 'Male', '167', '50');
+INSERT INTO member_info VALUES('110B13115', 'Male', '170', '57');
+INSERT INTO member_info VALUES('110B13123', 'Female', '155', '79');
+INSERT INTO member_info VALUES('110B13382', 'Female', '163', '49');
---
+++ 會員資料表.sql
@@ -0,0 +1,7 @@
+CREATE TABLE member_info
+
+(mbr_id VARCHAR(9) NOT NULL, 
+性別 VARCHAR(100) NOT NULL, 
+身高 CHAR(3) NOT NULL, 
+體重 CHAR(3) NOT NULL, 
+PRIMARY KEY(mbr_id))
